9a591c651ab0ced5c32d505bbaf2ab35cc27c669
Bruno Filipe Ramos Martins <brunofiliperamos@hotmail.com>
2018-06-01 17:54:49 -0300

    Pagina sobre com apresentacao mais detalhada

--- resources/views/pages/sobre.blade.php
+++ resources/views/pages/sobre.blade.php
@@ -6,18 +6,40 @@
     <div class="row justify-content-md-center">
         <div class="col-md-8">
             <h1 style="font-size: 100px; margin-left: -100px">
-                Sobre nos
-            </h1>
+                Sobre
             <h4 style="font-size: 25px">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <p>Ferramentas utilizadas</p>
+                <i class="fab fa-html5"style="font-size: 5em;"></i>
+                <i class="fab fa-css3-alt"style="font-size: 5em;"></i>
+                <i class="fab fa-laravel"style="font-size: 5em;"></i>
+                <i class="fab fa-php"style="font-size: 5em;"></i>
+                <i class="fab fa-vuejs"style="font-size: 5em;"></i>
+
+                <p>Compativel com</p>
+                <i class="fab fa-apple"style="font-size: 3em;"></i>
+                <i class="fab fa-android"style="font-size: 3em;"></i>
+                <i class="fab fa-windows"style="font-size: 3em;"></i>
+                <p>
+                <i class="fab fa-firefox"style="font-size: 3em;"></i>
+                <i class="fab fa-chrome"style="font-size: 3em;"></i>
+                <i class="fab fa-edge"style="font-size: 3em;"></i>
+                <i class="fab fa-safari"style="font-size: 3em;"></i>
+                <i class="fab fa-internet-explorer"style="font-size: 3em;"></i>
             </h4>
-            
+            </h1>
+        </div>
+        
+        <div class="col-md-3 col-md-offset-1"><!--Adiciona barra ao lado direito do site  -->
+            <div class="card-header"style="margin-top:30px; border: 5px solid #000000; background-color: #FFFFFF">
+                <h2><i class="fas fa-link"></i>Links</h2>
+                <a href="https://github.com/brunodhr"><i class="fab fa-github" style="font-size: 2em;"></i></a>
+                <a href="https://facebook.com/brunofilipe01"><i class="fab fa-facebook"style="font-size: 2em;"></i></a>
+                <a href="https://instagram.com/brunofilipe01"><i class="fab fa-instagram"style="font-size: 2em;"></i></a>
+                <a href="https://twitter.com/brunofilipe01"><i class="fab fa-twitter"style="font-size: 2em;"></i></a>
+                <a href="https://www.linkedin.com/in/brunodhr"><i class="fab fa-linkedin"style="font-size: 2em;"></i></a>
+                <a href="https://laravelisthelimit.slack.com/"><i class="fab fa-slack"style="font-size: 2em;"></i></a>
             </div>
+        </div>
     </div>
 </div>
 @endsection
